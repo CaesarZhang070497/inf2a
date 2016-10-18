a9932a30d7889958958fc12456b99a80cdf6c939
veniversum <hello@veniversum.me>
2016-10-18 16:39:05 +0100

    Initial commit with Assignment 1 templates

---
+++ assignment1/src/MH_Parser.java
@@ -0,0 +1,65 @@
+
+// File:   MH_Parser.java
+// Date:   October 2013, modified October 2016.
+
+// Java template file for parser component of Informatics 2A Assignment 1.
+// Students should add a method body for tableEntry, implementing the LL(1) parse table for Micro-Haskell.
+
+
+import java.io.* ;
+ 
+class MH_Parser extends GenParser implements PARSER {
+
+    String startSymbol() {return "#Prog" ;}
+
+    // Right hand sides of all productions in grammar:
+
+    static String[] epsilon              = new String[] { } ;
+    static String[] Decl_Prog            = new String[] {"#Decl", "#Prog"} ;
+    static String[] TypeDecl_TermDecl    = new String[] {"#TypeDecl", "#TermDecl"} ;
+    static String[] VAR_has_Type         = new String[] {"VAR", "::", "#Type", ";"} ;
+    static String[] Type1_TypeOps        = new String[] {"#Type1", "#TypeOps"} ;
+    static String[] arrow_Type           = new String[] {"->", "#Type"} ;
+    static String[] Integer              = new String[] {"Integer"} ;
+    static String[] Bool                 = new String[] {"Bool"} ;
+    static String[] lbr_Type_rbr         = new String[] {"(", "#Type", ")"} ;
+    static String[] VAR_Args_eq_Exp      = new String[] {"VAR", "#Args", "=", "#Exp", ";"} ;
+    static String[] VAR_Args             = new String[] {"VAR", "#Args"} ;
+    static String[] Exp1                 = new String[] {"#Exp1"} ;
+    static String[] if_then_else         = new String[] {"if", "#Exp", "then", "#Exp", "else", "#Exp"} ;
+    static String[] Exp2_Op1             = new String[] {"#Exp2", "#Op1"} ;
+    static String[] eqeq_Exp2            = new String[] {"==", "#Exp2"} ;
+    static String[] lteq_Exp2            = new String[] {"<=", "#Exp2"} ;
+    static String[] Exp3_Ops2            = new String[] {"#Exp3", "#Ops2"} ;
+    static String[] plus_Exp3_Ops2       = new String[] {"+", "#Exp3", "#Ops2"} ;
+    static String[] minus_Exp3_Ops2      = new String[] {"-", "#Exp3", "#Ops2"} ;
+    static String[] Exp4_Ops3            = new String[] {"#Exp4", "#Ops3"} ;
+    static String[] VAR                  = new String[] {"VAR"} ;
+    static String[] NUM                  = new String[] {"NUM"} ;
+    static String[] BOOLEAN              = new String[] {"BOOLEAN"} ;
+    static String[] lbr_Exp_rbr          = new String[] {"(", "#Exp", ")"} ;
+
+    // May add auxiliary methods here if desired
+
+    String[] tableEntry (String nonterm, String tokClass) {
+
+        // Add code here
+
+    }
+}
+
+
+// For testing
+
+class MH_ParserDemo {
+
+    static PARSER MH_Parser = new MH_Parser() ;
+
+    public static void main (String[] args) throws Exception {
+	Reader reader = new BufferedReader (new FileReader (args[0])) ;
+	LEX_TOKEN_STREAM MH_Lexer = 
+	    new CheckedSymbolLexer (new MH_Lexer (reader)) ;
+	TREE theTree = MH_Parser.parseTokenStream (MH_Lexer) ;
+    }
+}
+
