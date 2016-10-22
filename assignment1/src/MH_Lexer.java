7e564a8ce4ff6f89e127c665f30814653ff9d308
veniversum <hello@veniversum.me>
2016-10-22 17:27:01 +0100

    Bugfix in MH_Lexer.java
    Change deadStates from -1 to numberOfStates() - 1
    Fix TokAcceptor class

--- assignment1/src/MH_Lexer.java
+++ assignment1/src/MH_Lexer.java
@@ -24,12 +24,12 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
         int nextState(int state, char input) {
             switch (state) {
                 case 0:
-                    return CharTypes.isSmall(input) ? 1 : -1;
+                    return CharTypes.isSmall(input) ? 1 : numberOfStates() - 1;
                 case 1:
                     return CharTypes.isSmall(input) || CharTypes.isLarge(input) || CharTypes.isDigit(
-                            input) || input == '\'' ? 1 : -1;
+                            input) || input == '\'' ? 1 : numberOfStates() - 1;
                 default:
-                    return -1;
+                    return numberOfStates() - 1;
 
             }
         }
@@ -41,7 +41,7 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         int deadState() {
-            return -1;
+            return numberOfStates() - 1;
         }
     }
 
@@ -53,20 +53,20 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         public int numberOfStates() {
-            return 0;
+            return 4;
         }
 
         @Override
         int nextState(int state, char input) {
             switch (state) {
                 case 0:
-                    return input == 0 ? 1 : CharTypes.isDigit(input) ? 2 : -1;
+                    return input == 0 ? 1 : CharTypes.isDigit(input) ? 2 : numberOfStates() - 1;
                 case 1:
-                    return -1;
+                    return numberOfStates() - 1;
                 case 2:
-                    return CharTypes.isDigit(input) ? 2 : -1;
+                    return CharTypes.isDigit(input) ? 2 : numberOfStates() - 1;
                 default:
-                    return -1;
+                    return numberOfStates() - 1;
             }
         }
 
@@ -77,7 +77,7 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         int deadState() {
-            return -1;
+            return numberOfStates() - 1;
         }
     }
 
@@ -89,30 +89,30 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         public int numberOfStates() {
-            return 9;
+            return 10;
         }
 
         @Override
         int nextState(int state, char input) {
             switch (state) {
                 case 0:
-                    return input == 'T' ? 1 : input == 'F' ? 4 : -1;
+                    return input == 'T' ? 1 : input == 'F' ? 4 : numberOfStates() - 1;
                 case 1:
-                    return input == 'r' ? 2 : -1;
+                    return input == 'r' ? 2 : numberOfStates() - 1;
                 case 2:
-                    return input == 'u' ? 3 : -1;
+                    return input == 'u' ? 3 : numberOfStates() - 1;
                 case 3:
-                    return input == 'e' ? 8 : -1;
+                    return input == 'e' ? 8 : numberOfStates() - 1;
                 case 4:
-                    return input == 'a' ? 5 : -1;
+                    return input == 'a' ? 5 : numberOfStates() - 1;
                 case 5:
-                    return input == 'l' ? 6 : -1;
+                    return input == 'l' ? 6 : numberOfStates() - 1;
                 case 6:
-                    return input == 's' ? 7 : -1;
+                    return input == 's' ? 7 : numberOfStates() - 1;
                 case 7:
-                    return input == 'e' ? 8 : -1;
+                    return input == 'e' ? 8 : numberOfStates() - 1;
                 default:
-                    return -1;
+                    return numberOfStates() - 1;
             }
         }
 
@@ -123,7 +123,7 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         int deadState() {
-            return -1;
+            return numberOfStates() - 1;
         }
     }
 
@@ -142,11 +142,11 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
         int nextState(int state, char input) {
             switch (state) {
                 case 0:
-                    return CharTypes.isSymbolic(input) ? 1 : -1;
+                    return CharTypes.isSymbolic(input) ? 1 : numberOfStates() - 1;
                 case 1:
-                    return CharTypes.isSymbolic(input) ? 1 : -1;
+                    return CharTypes.isSymbolic(input) ? 1 : numberOfStates() - 1;
                 default:
-                    return -1;
+                    return numberOfStates() - 1;
             }
         }
 
@@ -157,14 +157,14 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         int deadState() {
-            return -1;
+            return numberOfStates() - 1;
         }
     }
 
     static class WhitespaceAcceptor extends GenAcceptor implements DFA {
         @Override
         public String lexClass() {
-            return "whitespace";
+            return "";
         }
 
         @Override
@@ -176,13 +176,14 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
         int nextState(int state, char input) {
             switch (state) {
                 case 0:
-                    return CharTypes.isWhitespace(input) ? 1 : -1;
+                    return CharTypes.isWhitespace(input) ? 1 : numberOfStates() - 1;
                 case 1:
-                    return CharTypes.isWhitespace(input) ? 1 : -1;
+                    return CharTypes.isWhitespace(input) ? 1 : numberOfStates() - 1;
                 default:
-                    return -1;
+                    return numberOfStates() - 1;
             }
         }
+
         @Override
         boolean accepting(int state) {
             return state == 1;
@@ -190,36 +191,36 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         int deadState() {
-            return -1;
+            return numberOfStates() - 1;
         }
     }
 
     static class CommentAcceptor extends GenAcceptor implements DFA {
         @Override
         public String lexClass() {
-            return "comment";
+            return "";
         }
 
         @Override
         public int numberOfStates() {
-            return 5;
+            return 6;
         }
 
         @Override
         int nextState(int state, char input) {
             switch (state) {
                 case 0:
-                    return input == '-' ? 1 : -1;
+                    return input == '-' ? 1 : numberOfStates() - 1;
                 case 1:
-                    return input == '-' ? 2 : -1;
+                    return input == '-' ? 2 : numberOfStates() - 1;
                 case 2:
                     return input == '-' ? 2 : 3;
                 case 3:
-                    return !(CharTypes.isSymbolic(input) || CharTypes.isNewline(input)) ? 4 : -1;
+                    return !(CharTypes.isSymbolic(input) || CharTypes.isNewline(input)) ? 4 : numberOfStates() - 1;
                 case 4:
-                    return !CharTypes.isNewline(input) ? 4 : -1;
+                    return !CharTypes.isNewline(input) ? 4 : numberOfStates() - 1;
                 default:
-                    return -1;
+                    return numberOfStates() - 1;
             }
         }
 
@@ -230,7 +231,7 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         int deadState() {
-            return -1;
+            return numberOfStates() - 1;
         }
     }
 
@@ -246,17 +247,17 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         public String lexClass() {
-            return null;
+            return tok;
         }
 
         @Override
         public int numberOfStates() {
-            return tokLen + 1;
+            return tokLen + 2;
         }
 
         @Override
         int nextState(int state, char input) {
-            return tok.charAt(state) == input ? state + 1 : -1;
+            return (state < tokLen) && (tok.charAt(state) == input) ? state + 1 : numberOfStates() - 1;
         }
 
         @Override
@@ -266,13 +267,47 @@ class MH_Lexer extends GenLexer implements LEX_TOKEN_STREAM {
 
         @Override
         int deadState() {
-            return -1;
+            return numberOfStates() - 1;
         }
 
     }
 
     // add definition of MH_acceptors here
-    private static final DFA[] MH_acceptors = {};
+    private static final DFA varAcceptor;
+    private static final DFA numAcceptor;
+    private static final DFA booleanAcceptor;
+    private static final DFA symAcceptor;
+    private static final DFA whitespaceAcceptor;
+    private static final DFA commentAcceptor;
+    private static final DFA integerKeywordAcceptor;
+    private static final DFA boolKeywordAcceptor;
+    private static final DFA ifKeywordAcceptor;
+    private static final DFA thenKeywordAcceptor;
+    private static final DFA elseKeywordAcceptor;
+    private static final DFA leftBracketKeywordAcceptor;
+    private static final DFA rightBracketKeywordAcceptor;
+    private static final DFA semicolonKeywordAcceptor;
+    private static final DFA[] MH_acceptors;
+
+    static {
+        varAcceptor = new VarAcceptor();
+        numAcceptor = new NumAcceptor();
+        booleanAcceptor = new BooleanAcceptor();
+        symAcceptor = new SymAcceptor();
+        whitespaceAcceptor = new WhitespaceAcceptor();
+        commentAcceptor = new CommentAcceptor();
+        integerKeywordAcceptor = new TokAcceptor("Integer");
+        boolKeywordAcceptor = new TokAcceptor("Bool");
+        ifKeywordAcceptor = new TokAcceptor("if");
+        thenKeywordAcceptor = new TokAcceptor("then");
+        elseKeywordAcceptor = new TokAcceptor("else");
+        leftBracketKeywordAcceptor = new TokAcceptor("(");
+        rightBracketKeywordAcceptor = new TokAcceptor(")");
+        semicolonKeywordAcceptor = new TokAcceptor(";");
+        MH_acceptors = new DFA[]{integerKeywordAcceptor, boolKeywordAcceptor, ifKeywordAcceptor, thenKeywordAcceptor,
+                elseKeywordAcceptor, leftBracketKeywordAcceptor, rightBracketKeywordAcceptor, semicolonKeywordAcceptor,
+                booleanAcceptor, numAcceptor, varAcceptor, commentAcceptor, symAcceptor, whitespaceAcceptor};
+    }
 
     MH_Lexer(Reader reader) {
         super(reader, MH_acceptors);
